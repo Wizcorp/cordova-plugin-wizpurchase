bbc30c242f21c02ca1324912c7eccadc4a5fad72
jrouault <jrouault@wizcorp.jp>
2016-03-30 14:03:07 +0900

    Clean repo to match classic Cordova plugin structure, rename plugin to cordova-plugin-wizpurchase, add package.json for npm publication

---
+++ www/WizPurchase.js
@@ -0,0 +1,40 @@
+var exec = require("cordova/exec");
+
+var WizPurchase = {
+	getProductDetails: function (productIds, s, f) {
+		if (!productIds) {
+			return s();
+		}
+		if (!Array.isArray(productIds)) {
+			productIds = [ productIds ];
+		}
+		exec(s, f, "WizPurchase", "getProductDetails", [ productIds ]);
+	},
+
+	makePurchase: function(productId, s, f) {
+		if (!productId) {
+			return f("noProductId");
+		}
+		exec(s, f, "WizPurchase", "makePurchase", [ productId ]);
+	},
+
+	finishPurchase: function(productId, isConsumable, s, f) {
+		if (!productId) {
+			return s();
+		}
+		if (typeof isConsumable !== 'boolean') {
+			return f(WizPurchaseError.ARGS_TYPE_MISMATCH);
+		}
+		exec(s, f, "WizPurchase", "finishPurchase", [ productId, isConsumable ]);
+	},
+
+	getPendingPurchases: function (s, f) {
+		exec(s, f, "WizPurchase", "getPendingPurchases", []);
+	},
+
+	restoreAllPurchases: function (s, f) {
+		exec(s, f, "WizPurchase", "restoreAllPurchases", []);
+	}
+};
+
+module.exports = WizPurchase;
